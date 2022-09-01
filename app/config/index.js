6132da0fc9959dd0a80cf5490acb0d72b7e59117
steve-dickinson <steve.dickinson@defra.gov.uk>
2022-09-01 15:44:51 +0100

    Add port to config :(

--- app/config/index.js
+++ app/config/index.js
@@ -2,12 +2,14 @@ const Joi = require('joi')
 const messageQueueConfig = require('./message-queue')
 
 const schema = Joi.object({
+  port: Joi.number().default(3005),
   env: Joi.string().valid('development', 'test', 'production').default('development'),
   isDev: Joi.boolean().default(false),
   sendPaymentRequest: Joi.boolean().default(true)
 })
 
 const config = {
+  port: process.env.PORT,
   env: process.env.NODE_ENV,
   isDev: process.env.NODE_ENV === 'development',
   sendPaymentRequest: process.env.SEND_PAYMENT_REQUEST
