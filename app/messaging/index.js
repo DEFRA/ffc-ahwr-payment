4cbc855f9b920363a95ca017c2c50ca156ec30bf
steve-dickinson <steve.dickinson@defra.gov.uk>
2022-09-05 14:55:38 +0100

    Update postgres port

--- app/messaging/index.js
+++ app/messaging/index.js
@@ -8,20 +8,20 @@ let paymentActionReceiver
 
 const start = async () => {
   console.info('Starting message receivers')
-  console.info('Application payment request queue: ' + applicationPaymentRequestQueue)
-  console.info('Payment response subscription: ' + paymentResponseSubscription)
+  console.info('Application payment request queue: ' + JSON.stringify(applicationPaymentRequestQueue.address))
+  console.info('Payment response subscription: ' + JSON.stringify(paymentResponseSubscription.topic) + ' ' + JSON.stringify(paymentResponseSubscription.address))
 
   const applicationClaimAction = message => processApplicationPaymentRequest(message, applicationClaimReceiver)
   applicationClaimReceiver = new MessageReceiver(applicationPaymentRequestQueue, applicationClaimAction)
   await applicationClaimReceiver.subscribe()
 
-  console.info(`Ready to receive messages on ${applicationPaymentRequestQueue}`)
+  console.info(`Ready to receive messages on ${JSON.stringify(applicationPaymentRequestQueue.address)}`)
 
   const paymentRequestAction = message => processPaymentResponse(message, paymentActionReceiver)
   paymentActionReceiver = new MessageReceiver(paymentResponseSubscription, paymentRequestAction)
   await paymentActionReceiver.subscribe()
 
-  console.info(`Ready to receive messages on subscription ${paymentResponseSubscription}`)
+  console.info(`Ready to receive messages on subscription ${JSON.stringify(paymentResponseSubscription.address)}`)
 }
 
 const stop = async () => {
