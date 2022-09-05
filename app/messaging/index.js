6b0cf53b5074514ac694335d358e53e3f1083c5f
steve-dickinson <steve.dickinson@defra.gov.uk>
2022-09-05 14:48:16 +0100

    update messaging

--- app/messaging/index.js
+++ app/messaging/index.js
@@ -7,15 +7,21 @@ let applicationClaimReceiver
 let paymentActionReceiver
 
 const start = async () => {
+  console.info('Starting message receivers')
+  console.info('Application payment request queue: ' + applicationPaymentRequestQueue)
+  console.info('Payment response subscription: ' + paymentResponseSubscription)
+
   const applicationClaimAction = message => processApplicationPaymentRequest(message, applicationClaimReceiver)
   applicationClaimReceiver = new MessageReceiver(applicationPaymentRequestQueue, applicationClaimAction)
   await applicationClaimReceiver.subscribe()
+
+  console.info(`Ready to receive messages on ${applicationPaymentRequestQueue}`)
 
   const paymentRequestAction = message => processPaymentResponse(message, paymentActionReceiver)
   paymentActionReceiver = new MessageReceiver(paymentResponseSubscription, paymentRequestAction)
   await paymentActionReceiver.subscribe()
 
-  console.info('Ready to receive messages')
+  console.info(`Ready to receive messages on subscription ${paymentResponseSubscription}`)
 }
 
 const stop = async () => {
