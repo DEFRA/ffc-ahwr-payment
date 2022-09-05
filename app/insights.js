c6f0e1329b1c3e5ce9ab3b09bb42d6acaa44f093
steve-dickinson <steve.dickinson@defra.gov.uk>
2022-09-05 16:20:17 +0100

    Loggimg in appInsights

--- app/insights.js
+++ app/insights.js
@@ -5,7 +5,9 @@ function setup () {
     appInsights.setup().start()
     console.log('App Insights Running')
     const cloudRoleTag = appInsights.defaultClient.context.keys.cloudRole
+    console.log('App Insights Running - cloudrole', cloudRoleTag)
     const appName = process.env.APPINSIGHTS_CLOUDROLE
+    console.log('App Insights Running - appName', appName)
     appInsights.defaultClient.context.tags[cloudRoleTag] = appName
   } else {
     console.log('App Insights Not Running!')
