516a4f25d63f2cc297e4f67be15b2e1c0ccbc985
liamwilson-kainos <liam.wilson@kainos.com>
2023-06-13 17:30:36 +0100

    added coverage

---
+++ test/unit/app/index.test.js
@@ -0,0 +1,23 @@
+describe('Index test', () => {
+    const mockMessaging = require('../../../app/messaging')
+    const mockCreateServer = require('../../../app/server')
+    const mockStartServerFunction = jest.fn().mockImplementation(() => 'running')
+    jest.mock('../../../app/messaging')
+    jest.mock('../../../app/insights')
+    jest.mock('../../../app/server')
+    mockCreateServer.mockResolvedValue({
+      info: {
+        uri: 'http://localhost:3000'
+      },
+      start: mockStartServerFunction
+    })
+    beforeEach(() => {
+      jest.clearAllMocks()
+    })
+    test('is started when env var exists', async () => {
+      await require('../../../app/index')
+      expect(mockMessaging.start).toHaveBeenCalledTimes(1)
+      expect(mockStartServerFunction).toHaveBeenCalledTimes(1)
+    })
+  })
+  
