3a190f1f3f4462dff1b60256f5dc58b193881b85
elamin.fadlalla <elamin.fadlalla@capgemini.com>
2024-05-27 15:05:08 +0100

    add app test mock  setup for storage account and message queue

--- test/setup.js
+++ test/setup.js
@@ -4,6 +4,6 @@ jest.mock('../app/config/storage', () => ({
   storageAccount: 'mockStorageAccount'
 }))
 
-jest.mock('../app/config/storage', () => ({
+jest.mock('../app/config/message-queue.js', () => ({
   storageAccount: 'mockStorageAccount'
 }))
